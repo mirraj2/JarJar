4dabc644a0c3ca964b0cf909da9b0910a39fa76b
Jason Mirra <mirraj2@gmail.com>
2021-06-04 12:38:11 -0500

    save jar to temp dir

--- src/jarjar/JarJarCommandLine.java
+++ src/jarjar/JarJarCommandLine.java
@@ -1,36 +1,17 @@
 package jarjar;
 
 import ox.File;
-import ox.HttpRequest;
 import ox.Json;
-import ox.x.XList;
 
 public class JarJarCommandLine {
 
   private void run(Json json) {
     json.log();
-    File.temp(temp -> {
-      JarJar.project(File.ofPath(json.get("project"))).main(json.get("main")).build(temp);
-      uploadToSignedUrl(json.getJson("signedUrl"), "application/java-archive", temp);
-    });
-  }
-
-  private void uploadToSignedUrl(Json signedUrl, String contentType, File file) {
-    HttpRequest request = HttpRequest.post(signedUrl.get("uploadUrl")).chunk(4_000_000);
-    XList<Json> formFields = signedUrl.getJson("formFields").asJsonArray();
-    formFields.forEach(f -> {
-      f.forEach((k, v) -> {
-        request.part(k, (String) v);
-      });
-    });
-    request.part("Content-Type", contentType)
-        .part("file", file.inputStream())
-        .checkStatus();
+    JarJar.project(File.ofPath(json.get("project"))).main(json.get("main")).build(File.ofPath(json.get("target")));
   }
 
   public static void main(String[] args) {
     new JarJarCommandLine().run(new Json(args[0]));
   }
-
 
 }
